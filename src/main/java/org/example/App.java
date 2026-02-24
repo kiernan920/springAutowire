3989f5dde95268a790bbc86c6b30aaa7f4630e5f
John Kiernan J <john.j.kiernan@ericsson.com>
2026-02-24 15:52:32 +0000

    Add rest of code and pom

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+            public static void main(String[] args) {
+                var context = new AnnotationConfigApplicationContext
+                        (ProjectConfig.class);
+                Person p =context.getBean(Person.class);
+                System.out.println("Person's name: " + p.getName());
+                System.out.println("Person's parrot: " + p.getParrot());
+
     }
 }
--- src/main/java/org/example/Parrot.java
+++ src/main/java/org/example/Parrot.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Parrot {
+
+    private String name = "KOKO";
+
+    public String getName() {
+        return name;
+    }
+
+    public Parrot setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Parrot{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
